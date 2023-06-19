cadd2d7cf199e7ca7863487bceb7400ccd3077f0
Keira-McLean_lmig <keira.mclean@libertymutual.com>
2023-06-19 11:13:05 -0400

    commit change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and create capstone project 2
         </a>
       </header>
     </div>
