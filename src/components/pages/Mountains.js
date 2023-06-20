4b45d6ce65837572bf7a00771525a3d612b43782
syclone1657 <syclone1657@gmail.com>
2023-06-20 11:08:19 -0400

    Update Mountains page

--- src/components/pages/Mountains.js
+++ src/components/pages/Mountains.js
@@ -1,11 +1,13 @@
-import React from 'react';
-
+import React from "react";
+import MountainList from "../../MountainList";
 
 export default function Mountain() {
-
-    return (
-        <>
-        <h1>This is Mountain Page</h1>
-        </>
-    )    
+  return (
+    <>
+      <h1>This is Mountain Page</h1>
+      <div>
+        <MountainList></MountainList>
+      </div>
+    </>
+  );
 }
