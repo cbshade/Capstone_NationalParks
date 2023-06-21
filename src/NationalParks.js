4bbd3ce28fee6684a06431d21351e2d26cc0a6cd
Chris Shade <cbshade@gmail.com>
2023-06-21 09:41:57 -0700

    table styling

--- src/NationalParks.js
+++ src/NationalParks.js
@@ -4,22 +4,18 @@ import NationalParksData from "../src/components/data/nationalparks.json";
 export default function NationalParks() {
   return (
     <>
-      <div>
-        {NationalParksData.parks.map((park, i) => {
-          return (
-            <div key={i}>
-              <tr>
-                <td>{park.LocationName}</td>
-                <td>{park.Address}</td>
-                <td>{park.City}</td>
-                <td>{park.State}</td>
-                <td>{park.ZipCode}</td>
-                <td>{park.Phone}</td>
-              </tr>
-            </div>
-          );
-        })}
-      </div>
+      {NationalParksData.parks.map((park, i) => {
+        return (
+          <tr>
+            <td>{park.LocationName}</td>
+            <td>{park.Address}</td>
+            <td>{park.City}</td>
+            <td>{park.State}</td>
+            <td>{park.ZipCode}</td>
+            <td>{park.Phone}</td>
+          </tr>
+        );
+      })}
     </>
   );
 }
--- src/components/pages/Parks.js
+++ src/components/pages/Parks.js
@@ -2,20 +2,25 @@ import React from "react";
 import NationalParks from "../../NationalParks";
 import LocationSelect from "../../LocationSelect";
 
+
 export default function Park() {
   return (
     <>
       <LocationSelect />
-      <table>
-        <tr>
-          <th>Location Name</th>
-          <th>Address</th>
-          <th>City</th>
-          <th>State</th>
-          <th>Zip</th>
-          <th>Phone</th>
-        </tr>
+      <table class="styled-table">
+        <thead>
+          <tr>
+            <th>Location Name</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Zip</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
         <NationalParks />
+        </tbody>
       </table>
     </>
   );
