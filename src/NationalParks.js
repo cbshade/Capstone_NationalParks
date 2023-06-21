c61555d8fdcbf097e9ac29da558ed945e2d7a8fb
Keira-McLean_lmig <keira.mclean@libertymutual.com>
2023-06-21 14:26:54 -0400

    update parks page

--- src/NationalParks.js
+++ src/NationalParks.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NationalParksData from "../src/components/data/nationalparks.json";
+import LocationsData from "../src/components/data/locations.json";
 
 
 const filteredLocations = NationalParksData.parks.filter(
@@ -9,6 +10,19 @@ const filteredLocations = NationalParksData.parks.filter(
 export default function NationalParks() {
   return (
     <>
+      <div>
+        <select>
+          <option value="" disabled default selected>
+            Select by Location
+          </option>
+          {LocationsData.map((location) => (
+            <option key={location} value={location}>
+              {location}
+            </option>
+          ))}
+        </select>
+      </div>
+
       {filteredLocations.map((park, i) => {
         return (
           <tr key={i}>
