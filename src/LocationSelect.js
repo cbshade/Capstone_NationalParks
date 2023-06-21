9a01a2707ff551644a4837a4b6cbe407fcae7f2a
syclone1657 <syclone1657@gmail.com>
2023-06-21 13:04:31 -0400

    Update LocationSelect.js
    Default value corrected

--- src/LocationSelect.js
+++ src/LocationSelect.js
@@ -4,6 +4,9 @@ import LocationsData from "../src/components/data/locations.json";
 export default function LocationSelect() {
   return (
     <select>
+      <option value="" disabled default selected>
+        Select by Location
+      </option>
       {LocationsData.map((location) => (
         <option key={location} value={location}>
           {location}
