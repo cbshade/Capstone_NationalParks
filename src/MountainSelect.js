6f80f29709bb29e58def353e33fb16f2303ec507
syclone1657 <syclone1657@gmail.com>
2023-06-21 10:32:40 -0400

    Removed coords

--- src/MountainSelect.js
+++ src/MountainSelect.js
@@ -1,58 +1,56 @@
 import React, { useState } from "react";
 import mountains from "../src/components/data/mountains.json";
 
-
-const mountainList = [mountains]
+const mountainList = [mountains];
 
 console.log(mountainList);
 
 function MountainSelect() {
-    const [filteredMountains, setFilteredMountains] = useState(mountainList)
+  const [filteredMountains, setFilteredMountains] = useState(mountainList);
 
-    const filterByName = name => {
-        setFilteredMountains(
-            mountainList[0].mountains.filter(mountain => mountain.name === name)
-        )
-    }
+  const filterByName = (name) => {
+    setFilteredMountains(
+      mountainList[0].mountains.filter((mountain) => mountain.name === name)
+    );
+  };
 
-    // Using Set to filter unique values
-    const names = Array.from(
-        new Set(mountainList[0].mountains.map(mountain => mountain.name))
-    )
+  // Using Set to filter unique values
+  const names = Array.from(
+    new Set(mountainList[0].mountains.map((mountain) => mountain.name))
+  );
 
-    return (
-        <div>
-            <select onChange={e => filterByName(e.target.value)}>
-                <option value="" disabled default selected>
-                    Select mountain
-                </option>
+  return (
+    <div>
+      <select onChange={(e) => filterByName(e.target.value)}>
+        <option value="" disabled default selected>
+          Select mountain
+        </option>
 
-                {names.map(name => {
-                    return <option key={name}>{name}</option>
-                })}
-            </select>
-            <ul>
-                {filteredMountains.map(mountain => {
-                    const { name, elevation, effort, img, desc, coords } = mountain
-                    return (
-                        <div key={name}>
-                            <div>
-                                <h2>{name}</h2>
-                                <img src={img} alt={name} />
-                                <p>Description: {desc}</p>
-                                <p>Effort: {effort}</p>
-                                <p>Elevation: {elevation}</p>
-                                <span>Latitude: {coords.lat}</span>
-                                <br></br>
-                                <span>Longitude: {coords.lng}</span>
-                            </div>
-                        </div>
-                    )
-                })}
-            </ul>
-        </div>
-    )
-};
-
+        {names.map((name) => {
+          return <option key={name}>{name}</option>;
+        })}
+      </select>
+      <ul>
+        {filteredMountains.map((mountain) => {
+          const { name, elevation, effort, img, desc } = mountain;
+          return (
+            <div key={name}>
+              <div>
+                <h2>{name}</h2>
+                <img src={img} alt={name} />
+                <p>Description: {desc}</p>
+                <p>Effort: {effort}</p>
+                <p>Elevation: {elevation}</p>
+                {/* <span>Latitude: {coords.lat}</span>
+                <br></br>
+                <span>Longitude: {coords.lng}</span> */}
+              </div>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
 
 export default MountainSelect;
