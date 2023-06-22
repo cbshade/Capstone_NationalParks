c341ab6127477e1b1e3d796612ecfe157b0ecad3
Keira-McLean_lmig <keira.mclean@libertymutual.com>
2023-06-22 10:24:25 -0400

    add search buttons

--- src/components/pages/Parks.js
+++ src/components/pages/Parks.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ParkTypeSelect from "../../ParkTypeSelect";
 import ParkLocationSelect from "../../ParkLocationSelect";
 
 
 export default function Park() {
+
+  const [searchOption, setSearchOption] = useState("ParkType");
+
+  const onOptionChange = e => {
+    setSearchOption(e.target.value);
+  }
+
   return (
     <>
-      <ParkTypeSelect />
-      <ParkLocationSelect />
+
+      <div className="App">
+        <h3>Select Search Option</h3>
+
+        <input type="radio" name="type" value="Location" id="location"
+          checked={searchOption === "Location"} onChange={onOptionChange} />
+        <label >By Location</label>
+
+        <input type="radio" name="type" value="ParkType" id="parktype"
+          checked={searchOption === "ParkType"} onChange={onOptionChange} />
+        <label >By Park Type</label>
+      </div>
+      <div>
+        {searchOption === 'ParkType' && <ParkTypeSelect />}
+        {searchOption === 'Location' && <ParkLocationSelect />}
+      </div>
+
     </>
   )
 }
