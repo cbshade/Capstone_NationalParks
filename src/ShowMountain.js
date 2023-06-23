1fc848702b7c15809591df7c19c5cc194e612553
Chris Shade <cbshade@gmail.com>
2023-06-23 07:52:00 -0700

    Google Maps on ShowMountains

--- src/ShowMountain.js
+++ src/ShowMountain.js
@@ -10,18 +10,30 @@ function ShowMountain(props) {
           const { name, elevation, effort, img, desc, coords } = mountain;
           return (
             <div key={name}>
-                <h2>{name}</h2>
-                <img src={img} alt={name} />
-                <p>Description: {desc}</p>
-                <p>Effort: {effort}</p>
-                <p>Elevation: {elevation}</p>
-                <p>Latitude: {coords.lat}</p>
-                <p>Longitude: {coords.lng}</p>
-                <SunInfo lat={coords.lat} lng={coords.lng}/>
+              <h2>{name}</h2>
+              <img src={img} alt={name} />
+              <p>Description: {desc}</p>
+              <p>Effort: {effort}</p>
+              <p>Elevation: {elevation}</p>
+              <p>Latitude: {coords.lat}</p>
+              <p>Longitude: {coords.lng}</p>
+              <SunInfo lat={coords.lat} lng={coords.lng} />
+
+
+              <iframe
+                title="GoogleMap"
+                width="600"
+                height="450"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                src={`https://maps.google.com/maps?q=${coords.lat},${coords.lng}&hl=en&z=14&output=embed`}
+              ></iframe>
             </div>
-          );
+      );
         })}
-      </ul>
+    </ul >
     </>
   );
 }
